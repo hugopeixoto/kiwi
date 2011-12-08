d23e4bce6bd5037b46917175de057a260db78065
Hugo Peixoto <hugo.peixoto@gmail.com>
2011-12-08 01:12:26 +0000

    Adds the http method enumeration type.

---
+++ http/method.h
@@ -0,0 +1,18 @@
+
+#ifndef KIWI_HTTP_METHOD_H_
+#define KIWI_HTTP_METHOD_H_
+
+namespace kiwi {
+  namespace http {
+    enum class Method {
+      GET,
+      POST,
+      PUT,
+      DELETE,
+      HEAD
+    };
+  }
+}
+
+#endif // KIWI_HTTP_METHOD_H_
+
