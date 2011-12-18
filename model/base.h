ecd252ed232c2c966990aee040f40d6b7026a991
Hugo Peixoto <hugo.peixoto@gmail.com>
2011-12-18 17:30:43 +0000

    Adds a dummy model::Base::all() method

--- model/base.h
+++ model/base.h
@@ -4,16 +4,24 @@
 
 #include <string>
 #include <map>
+#include <set>
 
 namespace kiwi {
   namespace model {
     template<typename Model>
     class Base {
     public:
+      typedef std::set<Model*> Set;
+
       static Model* find (const std::string& a_id);
+
+      static Set all ();
 
     protected:
       static std::map<std::string, Model*> cache_;
+
+    public:
+      static const char model_name_[];
     };
 
     template<typename Model>
@@ -29,16 +37,25 @@ namespace kiwi {
 
       // Load the thing from the database.
       Model* obj = NULL;
-      if (a_id == "1") obj = new Model("Kiwi first post");
-      if (a_id == "2") obj = new Model("Kiwi *second* post");
+      if (a_id == "1") obj = new Model(1, "Kiwi first post");
+      if (a_id == "2") obj = new Model(2, "Kiwi *second* post");
 
       return (cache_[a_id] = obj);
+    }
+
+    template<typename Model>
+    typename Base<Model>::Set Base<Model>::all ()
+    {
+      Base<Model>::Set set;
+      set.insert(find("1"));
+      set.insert(find("2"));
+      return set;
     }
 
     /**
      *Template static member instantiation 
      */
-    template <class Model>
+    template <typename Model>
     std::map<std::string, Model*> Base<Model>::cache_;
   }
 }
